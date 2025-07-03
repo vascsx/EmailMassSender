301d3a0203c3829a6aa909b8223aab834ad63b9e
vascsx <vasczn@gmail.com>
2025-07-03 13:29:11 -0300

    Add base project structure

---
+++ EmailAPI/Controllers/EmailController.cs
@@ -0,0 +1,37 @@
+﻿using EmailAPI.Model;
+using EmailAPI.Model.MongoDB;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace EmailAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMongoCollection<EmailModelMongoDB> _collection;
+
+        public EmailController(IConfiguration config)
+        {
+            var client = new MongoClient(config["MongoConnection"]);
+            var db = client.GetDatabase("EmailDB");
+            _collection = db.GetCollection<EmailModelMongoDB>("EmailQueue");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(EmailRequest request)
+        {
+            var model = new EmailModelMongoDB
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Subject = request.Subject,
+                Message = request.Message
+            };
+
+            await _collection.InsertOneAsync(model);
+            return Accepted();
+        }
+    }
+
+}
---
+++ EmailAPI/Model/MongoDB/EmailModelMongoDB.cs
@@ -0,0 +1,14 @@
+﻿namespace EmailAPI.Model.MongoDB
+{
+    public class EmailRequestMongoDB
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Message { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
